b06290f65cd04c27f3e5cb55b192c7c4dacbe333
praveenbabu <praveenbabu8497@gmail.com>
2025-11-03 22:16:11 +0530

    Finishing the Project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Task-Focus/',    // <-- exactly your repo name with slashes
 })
